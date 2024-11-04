e16e93bd81130e294f3e7f6a2d14857bfa2d1d1e
efe mantaroglu <efemantaroglu@gmail.com>
2024-11-04 22:16:22 +0300

    Train.sh takes inputs directly now.

--- train.sh
+++ train.sh
@@ -7,47 +7,10 @@
 #SBATCH --mem-per-gpu=20G
 #SBATCH --job-name=CNN+LSTM
 
-# Default values for the parameters
-epochs=32
-batch_size=256
-learning_rate=0.001
-config_name=config.yaml
-
-# Parse command-line arguments to override defaults
-while [ $# -gt 0 ]; do
-  case "$1" in
-    --epochs=*)
-      epochs="${1#*=}"
-      ;;
-    --batch_size=*)
-      batch_size="${1#*=}"
-      ;;
-    --learning_rate=*)
-      learning_rate="${1#*=}"
-      ;;
-    --num_of_points=*)
-      num_of_points="${1#*=}"
-      ;;
-    --config_name=*)
-      config_name="${1#*=}"
-      ;;
-    *)
-      echo "Unknown parameter: $1"
-      exit 1
-      ;;
-  esac
-  shift
-done
-
 cd /mnt/{your-username-here}/modular-train
 # Source the configuration file
 source configs/config.env
 export WANDB_API_KEY=$WANDB_API_KEY
 wandb login
 
-python src/modular_trainer_regression.py \
-  --config-name=${config_name}\
-  hydra.run.dir=. \
-  epochs=${epochs} \
-  batch_size=${batch_size} \
-  learning_rate=${learning_rate} \
+python src/modular_trainer_regression.py "$@"
